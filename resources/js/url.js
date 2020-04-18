8a524ca7ba1781415e5dd9ab99f81b00a0b058af
websturing <websturing.project@gmail.com>
2020-04-18 19:54:51 +0700

    change onlie url to new path

--- resources/js/url.js
+++ resources/js/url.js
@@ -2,9 +2,9 @@
 // const urlBase = "siJempol2020";
 // const urlWeb = "http://localhost/siJempol2020";
 
-const urlAxios = "http://sijempol.appkepri.com/sijempol/api";
+const urlAxios = "http://appkepri.com/sijempol/api";
 const urlBase = "sijempol";
-const urlWeb = "http://sijempol.appkepri.com/sijempol";
+const urlWeb = "http://appkepri.com/sijempol";
 
 
 
