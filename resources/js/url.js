d4ff4d0cea7ccd2eb8b9cba8b7aa793a5c4f8207
websturing <websturing.project@gmail.com>
2020-04-18 18:18:47 +0700

    change local to online

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,6 +1,13 @@
-const urlAxios = "http://localhost/siJempol2020/api";
-const urlBase = "siJempol2020";
-const urlWeb = "http://localhost/siJempol2020";
+// const urlAxios = "http://localhost/siJempol2020/api";
+// const urlBase = "siJempol2020";
+// const urlWeb = "http://localhost/siJempol2020";
+
+const urlAxios = "http://sijempol.appkepri.com/sijempol/api";
+const urlBase = "sijempol";
+const urlWeb = "http://sijempol.appkepri.com/sijempol";
+
+
+
 
 export default {
     urlAxios: urlAxios,
