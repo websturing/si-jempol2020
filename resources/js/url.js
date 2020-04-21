bda6c146be75a42cc83d308c86aa6ea3d00acb91
websturing <websturing.project@gmail.com>
2020-04-21 13:47:04 +0700

    local to online

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,10 +1,10 @@
-const urlAxios = "http://localhost/siJempol2020/api";
-const urlBase = "siJempol2020";
-const urlWeb = "http://localhost/siJempol2020";
+// const urlAxios = "http://localhost/siJempol2020/api";
+// const urlBase = "siJempol2020";
+// const urlWeb = "http://localhost/siJempol2020";
 
-// const urlAxios = "http://appkepri.com/sijempol/api";
-// const urlBase = "sijempol";
-// const urlWeb = "http://appkepri.com/sijempol";
+const urlAxios = "http://appkepri.com/sijempol/api";
+const urlBase = "sijempol";
+const urlWeb = "http://appkepri.com/sijempol";
 
 
 
